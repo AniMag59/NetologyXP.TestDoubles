77ef8718770083f4c717f01e620a6d696c764d4f
AniMag59 <la2xp1onerchar@rambler.ru>
2016-10-27 12:08:30 +0500

    Добавлены 2 теста с mock

--- test/whenBarmanWantsToPourWhiskyMockTests.js
+++ test/whenBarmanWantsToPourWhiskyMockTests.js
@@ -5,50 +5,30 @@ var Cupboard = require('../src/cupboard.js');
 var Client = require('../src/client.js');
 
 suite('Mock: when client ask 200 grams of whisky', function () {
-    var client = new Client();
     var drinkName = 'whisky';
-    setup(function () {
-
-    });
     suite('barman has enough', function () {
-        var cupboard;
-        var CupboardMock;
-        var barman;
-        setup(function () {
-            console.log('Barman setup');
-            cupboard = new Cupboard();
-            CupboardMock = sinon.mock(cupboard);
-            barman = new Barman(cupboard);
-        });
-        test('barman pour 200 grams of whisky', function () {
+         test('barman get whisky from cupboard', function () {
+            var client = new Client();
+            var cupboard = new Cupboard();
+            var CupboardMock = sinon.mock(cupboard);
+            var barman = new Barman(cupboard);
             var clientAskVolume = 200;
-            CupboardMock.expects('hasDrink').once().returns(true);
             CupboardMock.expects('getDrink').once().returns(200);
-            var volumeInGlass = barman.pour(drinkName, clientAskVolume, client);
-
+            var volumeInGlass =  barman.pour(drinkName, clientAskVolume, client);
             assert.equal(clientAskVolume, volumeInGlass);
             CupboardMock.restore();
             CupboardMock.verify();
         });
-
-
-        test('barman refuse client because he is drunken ', function() {
-            var first = barman.pour(drinkName, 200, client);
-            client.drink(first);
-
-            var second = barman.pour(drinkName, 50, client);
-
-            assert.equal(0, second);
+        test('barman checked that the client is drunk', function() {
+            var cupboard = new Cupboard();
+            var barman = new Barman(cupboard);
+            var client = new Client();
+            var clientMock = sinon.mock(client);
+            clientMock.expects('isDrunken').once().returns(true);
+            var drinked = barman.pour(drinkName, 50, client);
+            assert.equal(0, drinked);
+            clientMock.restore();
+            clientMock.verify();
         });
     });
-
-    suite('no whisky in bar', function () {
-        test('barman send SMS to the boss', function () {
-
-        });
-    });
-
-    teardown(function() {
-        console.log('teardown');
-    })
 });
